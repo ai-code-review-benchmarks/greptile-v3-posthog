f540c7408a1dc2f4e60b4d313d6ed2da38ed0ccd
Paweł Szczur <pawel@posthog.com>
2025-08-20 14:55:37 +0200

    squash query log archive

--- posthog/clickhouse/migrations/0131_add_team_id_alias_to_query_log_archive.py
+++ posthog/clickhouse/migrations/0131_add_team_id_alias_to_query_log_archive.py
@@ -1,11 +1,3 @@
-from posthog.clickhouse.client.connection import NodeRole
-from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
-from posthog.clickhouse.query_log_archive import ADD_TEAM_ID_ALIAS_COLUMN
+from infi.clickhouse_orm import migrations
 
-operations = [
-    run_sql_with_exceptions(
-        ADD_TEAM_ID_ALIAS_COLUMN,
-        node_role=NodeRole.ALL,
-        is_alter_on_replicated_table=True,
-    ),
-]
+operations: list[migrations.RunPython] = []
