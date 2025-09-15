a5ae229b121773581d6880c6c5fb18002adfae2a
Julian Bez <julian@posthog.com>
2025-09-15 14:21:38 +0200

    Fix assertDictContainsSubset removal

--- posthog/hogql_queries/ai/test/test_traces_query_runner.py
+++ posthog/hogql_queries/ai/test/test_traces_query_runner.py
@@ -446,15 +446,15 @@ class TestTracesQueryRunner(ClickhouseTestMixin, BaseTest):
         self.assertEqual(len(response.results), 1)
         self.assertEqual(response.results[0].id, "trace1")
         self.assertEqual(response.results[0].totalLatency, 10.5)
-        self.assertDictContainsSubset(
+        self.assertLessEqual(
             {
                 "$ai_latency": 10.5,
                 "$ai_provider": "posthog",
                 "$ai_model": "hog-destroyer",
                 "$ai_http_status": 200,
                 "$ai_base_url": "https://us.posthog.com",
-            },
-            response.results[0].events[0].properties,
+            }.items(),
+            response.results[0].events[0].properties.items(),
         )
 
     @freeze_time("2025-01-01T00:00:00Z")
